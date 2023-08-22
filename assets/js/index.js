78670c53a45bf4de7be62dd9300d61b23e7f539a
Oleksiy Lazarenko <alexusunit@gmail.com>
2023-08-22 20:31:29 +0100

    adds shuffleArray and selectRandomArrayIndex functions, assigns random numbers to cards with no repition

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
 	const cardBoard = document.querySelector('#card-board');
 	let cardsHTML = '';
-	for (let index = 0; index < 20; index++) {
+	let arrNumbers = [];
+	let numberOfCards = 20;
+
+  for (let index = 0; index < numberOfCards; index++) {
+    arrNumbers.push(index + 1);
+  }
+
+  arrNumbers = shuffleArray(arrNumbers);
+
+	for (let index = 0; index < numberOfCards; index++) {
 		cardsHTML += 
 		`<div class="card-container">
 				<div class="card">
 					<div class="card-front">
-						${index}
+						${arrNumbers[index]}
 					</div>
 					<div class="card-back">
 					</div>
@@ -20,3 +29,21 @@ document.addEventListener("DOMContentLoaded", function() {
 		}
 	})
 })
+
+function selectRandomArrayIndex(arr) {
+  return Math.floor(Math.random() * arr.length);
+}
+
+function shuffleArray(arrOfNumbers) {
+  const arr = [...arrOfNumbers];
+  const newArr = [];
+  let newValue;
+  let randomIndex;
+  for (let index = 0; index < arrOfNumbers.length; index++) {
+    randomIndex = selectRandomArrayIndex(arr)
+    newValue = arr[randomIndex]
+    arr.splice(randomIndex, 1)
+    newArr.push(newValue)
+  }
+  return newArr
+}
