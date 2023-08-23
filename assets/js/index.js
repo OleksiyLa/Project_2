6845eae3632a0a33fa9f0b7212c3d46eefd0f019
Oleksiy Lazarenko <alexusunit@gmail.com>
2023-08-23 09:36:11 +0100

    creates generateArrayFromOneToArgumentNumber and generateCardsHTML functions, adds eventlistenr on button to ggenerate shuffled cards and adds ability to open those cards

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,49 +1,62 @@
 document.addEventListener("DOMContentLoaded", function() {
-	const cardBoard = document.querySelector('#card-board');
-	let cardsHTML = '';
-	let arrNumbers = [];
-	let numberOfCards = 20;
+  const cardBoard = document.querySelector('#card-board');
+  const startButton = document.querySelector('#start-button');
 
-  for (let index = 0; index < numberOfCards; index++) {
-    arrNumbers.push(index + 1);
-  }
+  let numberOfCards = 20;
+  let arrNumbers = generateArrayFromOneToArgumentNumber(numberOfCards);
+  cardBoard.innerHTML = generateCardsHTML(arrNumbers);
+  
 
-  arrNumbers = shuffleArray(arrNumbers);
 
-	for (let index = 0; index < numberOfCards; index++) {
-		cardsHTML += 
-		`<div class="card-container">
-				<div class="card">
-					<div class="card-front">
-						${arrNumbers[index]}
-					</div>
-					<div class="card-back">
-					</div>
-				</div>
-			</div>`;
-			}
-	cardBoard.innerHTML = cardsHTML
-	cardBoard.addEventListener("click", (e) => {
-		if(e.target.className === "card-back") {
-			e.target.parentNode.style.transform = 'rotateY(180deg)';
-		}
-	})
+  startButton.addEventListener("click", () => {
+    cardBoard.innerHTML = generateCardsHTML(shuffleArray(arrNumbers));
+    cardBoard.addEventListener("click", (e) => {
+      if(e.target.className === "card-back") {
+        e.target.parentNode.style.transform = 'rotateY(180deg)';
+      }
+    })
+  });
 })
 
 function selectRandomArrayIndex(arr) {
-  return Math.floor(Math.random() * arr.length);
+return Math.floor(Math.random() * arr.length);
 }
 
 function shuffleArray(arrOfNumbers) {
-  const arr = [...arrOfNumbers];
-  const newArr = [];
-  let newValue;
-  let randomIndex;
-  for (let index = 0; index < arrOfNumbers.length; index++) {
-    randomIndex = selectRandomArrayIndex(arr)
-    newValue = arr[randomIndex]
-    arr.splice(randomIndex, 1)
-    newArr.push(newValue)
+const arr = [...arrOfNumbers];
+const newArr = [];
+let newValue;
+let randomIndex;
+for (let index = 0; index < arrOfNumbers.length; index++) {
+  randomIndex = selectRandomArrayIndex(arr)
+  newValue = arr[randomIndex]
+  arr.splice(randomIndex, 1)
+  newArr.push(newValue)
+}
+return newArr
+}
+
+function generateArrayFromOneToArgumentNumber(numberOfCards) {
+  let arrNumbers = [];
+  for (let index = 0; index < numberOfCards; index++) {
+    arrNumbers.push(index + 1);
   }
-  return newArr
+  return arrNumbers
+}
+
+function generateCardsHTML(arrNumbers) {
+  let cardsHTML = '';
+  for (let index = 0; index < arrNumbers.length; index++) {
+    cardsHTML += 
+    `<div class="card-container">
+        <div class="card" id="card-${arrNumbers[index]}">
+          <div class="card-front">
+            ${arrNumbers[index]}
+          </div>
+          <div class="card-back">
+          </div>
+        </div>
+      </div>`;
+  }
+  return cardsHTML;
 }
